e0f8070a2411ca28a28adccc887fc8c570e7495e
Ammon Pike <ammon.pike@gmail.com>
2016-07-14 10:55:57 -0700

    UTF8 encoding fix

--- TAgJson.py
+++ TAgJson.py
@@ -10,9 +10,10 @@ jsonIn = sys.argv[1] #name json input file
 with open(jsonIn, 'r') as file:
     pair = json.load(file)
 #    for pair in jsonObj:
-#        print(pair)
+    print(pair)
     tempScore = normScore.d2Score(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
     pair['scores']['TAg'] = tempScore
-        
+
 with open(jsonIn, 'w') as file:
-    json.dump(pair, file)
+    json.dump(pair, file, ensure_ascii=False)
+
