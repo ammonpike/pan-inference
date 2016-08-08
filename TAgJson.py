ec6c6bb9662949482f7511531d6f7b20b6fb0f46
Ammon Pike <ammon.pike@gmail.com>
2016-08-08 15:07:29 -0700

    working on hybrid method

--- TAgJson.py
+++ TAgJson.py
@@ -5,22 +5,60 @@ import sys
 import json
 import panlex
 import normScore
+import time
 
 #Input file should have ex1, ex2 and score columns
 #ex items should have tt, ex and uid in them
 #score is hash with test as key, score as value
-
+i = 0
 jsonIn = sys.argv[1] #name json input file
 with open(jsonIn, 'r') as file:
     jsonObj = json.load(file)
     for pair in jsonObj:
-        tempScore = normScore.d2Score(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
-        pair['scores']['TAgADD'] = tempScore
-        tempScore = normScore.d2Geom(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
-        pair['scores']['TAgGeo'] = tempScore
-        tempScore = normScore.d1Conf(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
-        pair['scores']['d1Conf'] = tempScore
+        try:
+#            if 'TAgADD' in pair['scores']:
+#                continue
+#            else:
+#                tempScore = normScore.d2Score(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
+#                pair['scores']['TAgADD'] = tempScore
 
+#            if 'TAgCount' in pair['scores']:
+#                continue
+#            else:
+#                tempScore = normScore.d2noTQ(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
+#                pair['scores']['TAgCount'] = tempScore
+
+#            if 'TAgGeo' in pair['scores']:
+#                continue
+#            else:
+#                tempScore = normScore.d2Geom(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
+#                pair['scores']['TAgGeo'] = tempScore
+
+#            if 'd1Conf' in pair['scores']:
+#                continue
+#            else:
+#                tempScore = normScore.d1Conf(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
+#                pair['scores']['d1Conf'] = tempScore
+
+#            if 'd2Avg' in pair['scores']:
+#                continue
+#            else:
+#                tempScore = normScore.d2Avg(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'])
+#                pair['scores']['d2Avg'] = tempScore
+
+            if i < 10:
+                tempScore = normScore.d2Hybrid(pair['ex1']['uid'], pair['ex1']['tt'], pair['ex2']['uid'], pair['ex2']['tt'], pair['scores']['tr2qa'])
+                pair['scores']['d2Hybrid'] = tempScore
+            
+            i += 1
+            if i == 30:
+                time.sleep(3)
+                i = 10
+                print(tempScore)
+            
+        except:
+            print("error detected, sleeping.")
+            time.sleep(6)
 with open(jsonIn, 'w') as file:
     json.dump(jsonObj, file, ensure_ascii=False)
 
--- normScore.py
+++ normScore.py
@@ -5,20 +5,18 @@ import sys
 import json
 import panlex
 
-#Scores translation of words at distance 2 in PanLex database
-#Score is based on the ratio of the sum tq scores of the intersection
-#   of all distance 1 translations from the source word to lang3 and
-#   all translations from a target word in lang2 to lang3
-#   to the sum of the union of the same.
+#Methods for scoring translation of words at distance 2 in PanLex database
 #Function call requires source UID, source TT, target UID, target TT.
 #More efficient to use translationAgg.py for numerous calls
-#Super inefficient, but a good start. Does the job.
+#These are all really quite super inefficient, 
+#but they are designed to get the numbers
+#for testing. Optimization can happen later.
 
 
 def d1Conf(lang1, startWord, lang2, endWord):   
     queryParam = {"uid":lang2, 
                 "trtt":startWord, 
-                "truid": lang1, 
+                "trtt": lang1, 
                 "include":['trq'], 
                 "sort":"trq desc"}
     query = panlex.query('/ex', queryParam)
@@ -36,7 +34,51 @@ def d1Conf(lang1, startWord, lang2, endWord):
         return results[endWord]
     else:
         return 0.00
+        
+def d1ConfF(lang1, startWord, lv2, endWord):   
+    queryParam = {"lv":lv2, 
+                "trtt":startWord, 
+                "truid": lang1, 
+                "include":['trq'], 
+                "sort":"trq desc"}
+    query = panlex.query('/ex', queryParam)
 
+    totalTQ = 0
+    results = {}
+
+    for word in query['result']:
+        results[word['ex']] = word['trq']
+        totalTQ += word['trq']
+
+    for word in results:
+        results[word] = results[word] / totalTQ
+    if endWord in results:
+        return results[endWord]
+    else:
+        return 0.00
+
+def d1ConfB(lv1, startWord, lang2, endWord):   
+    queryParam = {"uid":lang2, 
+                "trex":startWord, 
+                "trlv": lv1, 
+                "include":['trq'], 
+                "sort":"trq desc"}
+    query = panlex.query('/ex', queryParam)
+
+    totalTQ = 0
+    results = {}
+
+    for word in query['result']:
+        results[word['tt']] = word['trq']
+        totalTQ += word['trq']
+
+    for word in results:
+        results[word] = results[word] / totalTQ
+    if endWord in results:
+        return results[endWord]
+    else:
+        return 0.00
+        
 def d2Score(lang1, startWord, lang2, endWord):
     paramStart = {"uid":lang2,
                 "tt":endWord,
@@ -49,32 +91,22 @@ def d2Score(lang1, startWord, lang2, endWord):
     d2 = panlex.query("/ex",paramStart)
     pathDict = {}
     for pathSet in d2['result'][0]['trpath']:
-        if pathSet[0]['ex2'] in pathDict:
-            pathDict[pathSet[0]['ex2']] += 1
+        if pathSet[0]['lv2'] in pathDict:
+            pathDict[pathSet[0]['lv2']] += 1
         else:
-            pathDict[pathSet[0]['ex2']] = 1
-    #retrieve lang codes for paths
-    paramLang = {"ex":list(pathDict.keys()),
-                 "include":"uid",}
-    LangSearch = panlex.query("/ex",paramLang)
-    interLing = {}
-    
-    for word in LangSearch['result']:
-        if word['uid'] in interLing:
-            interLing[word['uid']] += 1
-        else:
-            interLing[word['uid']] = 1
+            pathDict[pathSet[0]['lv2']] = 1
+
     #perform calculation on each intermediate language
     #perhaps put a safeguard discounting items with only one
     #intermediate item?
     tqSum = 0.00
     tqTotal = 0.00
-    ling = list(interLing.keys())
-    forwardParam = {"uid":ling,
+    ling = list(pathDict.keys())
+    forwardParam = {"lv":ling,
                     "truid":lang1,
                     "trtt":startWord,
                     "include":"trq",}
-    backwardParam = {"uid":ling,
+    backwardParam = {"lv":ling,
                     "truid":lang2,
                     "trtt":endWord,
                     "include":"trq",}
@@ -122,8 +154,85 @@ def d2Score(lang1, startWord, lang2, endWord):
 #            print(tqSum)
 #            scores[endWord] = tqSum / 18 #18 is max value of two dictionary entries
     return score
+
+def d2noTQ(lang1, startWord, lang2, endWord):
+    paramStart = {"uid":lang2,
+                "tt":endWord,
+                "truid":lang1,
+                "trtt":startWord,
+                "trdistance":2,
+                "include":['trq', 'trpath'],
+                "sort":'trq desc',
+                }
+    d2 = panlex.query("/ex",paramStart)
+    pathDict = {}
+
+    for pathSet in d2['result'][0]['trpath']:
+        if pathSet[0]['lv2'] in pathDict:
+            pathDict[pathSet[0]['lv2']] += 1
+        else:
+            pathDict[pathSet[0]['lv2']] = 1
+
+    #perform calculation on each intermediate language
+    #perhaps put a safeguard discounting items with only one
+    #intermediate item?
+    tqSum = 0.00
+    tqTotal = 0.00
+    ling = list(pathDict.keys())
+    forwardParam = {"lv":ling,
+                    "truid":lang1,
+                    "trtt":startWord,
+                    "include":"trq",}
+    backwardParam = {"lv":ling,
+                    "truid":lang2,
+                    "trtt":endWord,
+                    "include":"trq",}
+                    
+    frontCheck = panlex.query('/ex', forwardParam)
+    backCheck = panlex.query('/ex', backwardParam)
     
+    front = {}
+    back = {}
+
+    #sort results of search into dictionaries for comparison
+    #should I be using ex or tt? I used tt before but I guess if two
+    #words in diff languages had the same orthography...
+    for result in frontCheck['result']:
+#        print(result['tt'])
+        if result['ex'] in front:
+            front[result['ex']] += 1
+        else:
+            front[result['ex']] = 1
+            
+    for result in backCheck['result']:
+        if result['ex'] in back:
+            back[result['ex']] += 1
+        else:
+            back[result['ex']] = 1
+            
+    for word in front:
+        if word in back:
+            tqSum += front[word]
+            tqSum += back[word]
+#            if len(front) == 1:
+#                print(front[word])
+#            if len(back) == 1:
+#                print(back[word])
+        tqTotal += front[word]
+    for word in back:
+        tqTotal += back[word]    #theoretically, could the two above steps be combined?
+    #For example, add result to trq AND tqTotal
+    #Can items be 'in' JSON objects? Is it efficient?
+    score = tqSum / (tqTotal) #best smoothing number to date
+    #+ 1? That would curve the 1.0 scores (very) slightly.
+    #Actually, it would have greater impact the smaller the sample size. It could even be + 2 (one one-ranked for each side)
+#    if scores[endWord] == 1:
+#        if len(front) == 1 and len(back) == 1:
+#            print(tqSum)
+#            scores[endWord] = tqSum / 18 #18 is max value of two dictionary entries
+    return score
     
+#using geometric mean? Results seem to be worse though.
 def d2Geom(lang1, startWord, lang2, endWord):
     paramStart = {"uid":lang2,
                 "tt":endWord,
@@ -136,32 +245,19 @@ def d2Geom(lang1, startWord, lang2, endWord):
     d2 = panlex.query("/ex",paramStart)
     pathDict = {}
     for pathSet in d2['result'][0]['trpath']:
-        if pathSet[0]['ex2'] in pathDict:
-            pathDict[pathSet[0]['ex2']] += 1
+        if pathSet[0]['lv2'] in pathDict:
+            pathDict[pathSet[0]['lv2']] += 1
         else:
-            pathDict[pathSet[0]['ex2']] = 1
-    #retrieve lang codes for paths
-    paramLang = {"ex":list(pathDict.keys()),
-                 "include":"uid",}
-    LangSearch = panlex.query("/ex",paramLang)
-    interLing = {}
+            pathDict[pathSet[0]['lv2']] = 1
     
-    for word in LangSearch['result']:
-        if word['uid'] in interLing:
-            interLing[word['uid']] += 1
-        else:
-            interLing[word['uid']] = 1
-    #perform calculation on each intermediate language
-    #perhaps put a safeguard discounting items with only one
-    #intermediate item?
     tqSum = 0.00
     tqTotal = 0.00
-    ling = list(interLing.keys())
-    forwardParam = {"uid":ling,
+    ling = list(pathDict.keys())
+    forwardParam = {"lv":ling,
                     "truid":lang1,
                     "trtt":startWord,
                     "include":"trq",}
-    backwardParam = {"uid":ling,
+    backwardParam = {"lv":ling,
                     "truid":lang2,
                     "trtt":endWord,
                     "include":"trq",}
@@ -209,3 +305,120 @@ def d2Geom(lang1, startWord, lang2, endWord):
 #            print(tqSum)
 #            scores[endWord] = tqSum / 18 #18 is max value of two dictionary entries
     return score
+    
+#what about an average of d1conf * d1conf? Find both confidence, multiply, sum all paths and divide by number of paths?
+#need uid and tt of intermediate words
+def d2Avg(lang1, startWord, lang2, endWord):
+    paramStart = {"uid":lang2,
+                "tt":endWord,
+                "truid":lang1,
+                "trtt":startWord,
+                "trdistance":2,
+                "include":['trq', 'trpath'],
+                "sort":'trq desc',
+                }
+    d2 = panlex.query("/ex",paramStart)
+    
+    #perhaps do max() instead of average. average gets really low results (something Jonathan doesn't like)
+    count = 0
+    tqSum = 0.00
+    for word in d2['result'][0]['trpath']:
+        tempScore = d1ConfF(lang1, startWord, word[0]['lv2'], word[0]['ex2']) * d1ConfB(word[0]['lv2'], word[0]['ex2'], lang2, endWord)
+        count += 1
+        tqSum += tempScore
+    score = tqSum / count
+    return score
+#        if tempScore > count:
+#            count = tempScore
+#    return count
+
+def maxD2(lv1, lv2):
+    #retrieve d1 from lv csv file
+    #multiply and sqrt values
+    
+#function for combining tr2qa and d2Score
+#Basically do the d2Score, but while calculating note lv of intermediate languages
+#retrieve max d1 scores for each lv
+#divide tr2qa by maxD2
+def d2Hybrid(lang1, startWord, lang2, endWord, qaScore):
+    paramStart = {"uid":lang2,
+                "tt":endWord,
+                "truid":lang1,
+                "trtt":startWord,
+                "trdistance":2,
+                "include":['trq', 'trpath'],
+                "sort":'trq desc',
+                }
+    d2 = panlex.query("/ex",paramStart)
+    pathDict = {}
+    for pathSet in d2['result'][0]['trpath']:
+        if pathSet[0]['lv2'] in pathDict:
+            pathDict[pathSet[0]['lv2']] += 1
+        else:
+            pathDict[pathSet[0]['lv2']] = 1
+
+    #right here grab lv of each step
+    #find max D1 for each lv
+    #determine max D2 for translation pair
+    #also requires tr2qa score for pair
+    leftSum = 0
+    rightSum = 0
+    
+    #for intermediateLV in pathDict.keys():
+    #    get lv1 to intermediateLV max score
+    #    leftSum += tempLeft
+    #    get intermediateLV to lv2 max score
+    #    rightSum += tempRight
+    tr2Max = sqrt(leftSum * rightSum)
+    
+    norm2qa = qaScore / tr2Max
+    #perform calculation on each intermediate language
+    #perhaps put a safeguard discounting items with only one
+    #intermediate item?
+    tqSum = 0.00
+    tqTotal = 0.00
+    ling = list(pathDict.keys())
+    forwardParam = {"lv":ling,
+                    "truid":lang1,
+                    "trtt":startWord,
+                    "include":"trq",}
+    backwardParam = {"lv":ling,
+                    "truid":lang2,
+                    "trtt":endWord,
+                    "include":"trq",}
+                    
+    frontCheck = panlex.query('/ex', forwardParam)
+    backCheck = panlex.query('/ex', backwardParam)
+    
+    front = {}
+    back = {}
+
+    #sort results of search into dictionaries for comparison
+    #should I be using ex or tt? I used tt before but I guess if two
+    #words in diff languages had the same orthography...
+    for result in frontCheck['result']:
+#        print(result['tt'])
+        if result['ex'] in front:
+            front[result['ex']] += result['trq']
+        else:
+            front[result['ex']] = result['trq']
+            
+    for result in backCheck['result']:
+        if result['ex'] in back:
+            back[result['ex']] += result['trq']
+        else:
+            back[result['ex']] = result['trq']
+            
+    for word in front:
+        if word in back:
+            tqSum += front[word]
+            tqSum += back[word]
+        tqTotal += front[word]
+    for word in back:
+        tqTotal += back[word]
+    score = tqSum / (tqTotal + 6) #best smoothing number to date
+    
+    #finally calculate hybrid score
+    hybrid = score * norm2qa
+
+    return hybrid
